ab65f13c04af51f6d33555b9caa830621829096e
Ekateryna Nikitenko <ekaterinanikitenko940@gmail.com>
2022-10-20 08:38:45 +0300

    Solved FacadeDemo Pattern

--- JavaEE5_1/src/Circle.java
+++ JavaEE5_1/src/Circle.java
@@ -13,4 +13,11 @@ public class Circle extends Shape {
     public void draw() {
         drawAPI.drawCircle(radius,x, y);
     }
+
+    public static class GreenPen implements DrawAPI {
+
+        public void drawCircle(int radius, int x, int y) {
+            System.out.println("Drawing Circle color: green, radius: " + radius + ", x = " + x + ", y = " + y);
+        }
+    }
 }
